8d1a85b0ac029ce16f143d4c54a4b22ae59fb5cb
chenliang <chenliangsjtu@126.com>
2020-05-07 03:22:59 -0400

    Updates

--- precache-manifest.11d9e0714986a7a886e1593811cf0608.js
+++ precache-manifest.11d9e0714986a7a886e1593811cf0608.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9d2d9dac5f3c0b33678ef79b9e47241f",
+    "revision": "1ab2cd989223825a0e3db122146401dd",
     "url": "/liang/index.html"
   },
   {
-    "revision": "693f1a5d3f72531b4030",
+    "revision": "47a699ca2bb3013a34f8",
     "url": "/liang/static/css/main.5facb584.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/liang/static/js/2.70e65c8e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "693f1a5d3f72531b4030",
-    "url": "/liang/static/js/main.48c5ab97.chunk.js"
+    "revision": "47a699ca2bb3013a34f8",
+    "url": "/liang/static/js/main.a93520a0.chunk.js"
   },
   {
     "revision": "bb53119b96606fdcd430",
